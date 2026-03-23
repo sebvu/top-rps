2c7c915e045206e1a4f693c5aa5b0100b11eba61
Jester <jester.sebastiansantos@gmail.com>
2026-03-23 17:16:09 -0500

    Define getHumanChoice and computerScore

--- javascript.js
+++ javascript.js
@@ -1,10 +1,38 @@
-alert("hi");
+// get human choice
+// generate computer choice
+// playround, using switch
 
-let humanScore;
-let computerScore;
+main();
 
-function getComputerChoice() {}
+function main() {
+  let humanScore = getHumanChoice();
+  let computerScore = () => Math.floor(Math.random() * (4 - 1) + 1); // 1 - 3
 
-function getHumanChoice() {}
+  playRound(humanScore, computerScore());
+}
 
-function playRound() {}
+// Get human choice as a number
+function getHumanChoice() {
+  let prefixTextPrompt = "Make your option!";
+
+  while (true) {
+    let c = parseInt(
+      prompt(
+        `${prefixTextPrompt} Type in a number.\n1) Rock\n2) Paper\n3) Scissors`,
+      ),
+    );
+
+    switch (c) {
+      case 1:
+      case 2:
+      case 3:
+        return c;
+      default:
+        prefixTextPrompt = "Not a correct choice.";
+        break;
+    }
+    continue;
+  }
+}
+
+function playRound(humanScore, computerScore) {}
