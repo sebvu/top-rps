91ff862b36cc51b53f00a5ec546591987417eeae
Jester <jester.sebastiansantos@gmail.com>
2026-03-23 21:31:44 -0500

    Create RPS logic

--- javascript.js
+++ javascript.js
@@ -1,31 +1,79 @@
-// get human choice
-// generate computer choice
-// playround, using switch
+const ROCK_NUMBER = (COMPUTER_WIN_NUMBER = 1);
+const PAPER_NUMBER = (PLAYER_WIN_NUMBER = 2);
+const SCISSORS_NUMBER = (TIE_NUMBER = 3);
+const EXIT_NUMBER = (ERROR_NUMBER = 0);
 
 main();
 
 function main() {
-  let humanScore = getHumanChoice();
-  let computerScore = () => Math.floor(Math.random() * (4 - 1) + 1); // 1 - 3
+  let playerScore = 0;
+  let computerScore = 0;
+  let playerChoice;
+  let playCode;
 
-  playRound(humanScore, computerScore());
+  while (playerChoice != 0 || playCode != 0) {
+    let scoreContainer = [playerScore, computerScore];
+    playerChoice = getPlayerChoice(scoreContainer);
+    let computerChoice = getComputerChoice();
+    playCode = playRound(playerChoice, computerChoice);
+
+    let postfixStr = `\nPlayer Choice: ${getNamedAction(playerChoice)}\nComputer Choice: ${getNamedAction(computerChoice)}`;
+
+    switch (playCode) {
+      case COMPUTER_WIN_NUMBER:
+        alert(`Computer wins!${postfixStr}`);
+        computerScore++;
+        break;
+      case PLAYER_WIN_NUMBER:
+        alert(`Player wins!${postfixStr}`);
+        playerScore++;
+        break;
+      case TIE_NUMBER:
+        alert(`Both players tied!${postfixStr}`);
+        break;
+      case EXIT_NUMBER:
+        break;
+      default:
+        console.error("playCode is invalid in switch");
+        break;
+    }
+  }
+  alert("Thanks for playing! :)");
 }
 
-// Get human choice as a number
-function getHumanChoice() {
+function getNamedAction(ACTION_NUMBER) {
+  switch (ACTION_NUMBER) {
+    case ROCK_NUMBER:
+      return "Rock";
+    case PAPER_NUMBER:
+      return "Paper";
+    case SCISSORS_NUMBER:
+      return "Scissors";
+    default:
+      return "N/A";
+  }
+}
+
+function getComputerChoice() {
+  return Math.floor(Math.random() * (4 - 1) + 1); // 1 - 3
+}
+
+// Get player choice as a number
+function getPlayerChoice(scoreContainer) {
   let prefixTextPrompt = "Make your option!";
 
   while (true) {
     let c = parseInt(
       prompt(
-        `${prefixTextPrompt} Type in a number.\n1) Rock\n2) Paper\n3) Scissors`,
+        `${prefixTextPrompt}\nSCORE: Player: ${scoreContainer[0]}, Computer: ${scoreContainer[1]}\nType in a number.\n${ROCK_NUMBER}) Rock\n${PAPER_NUMBER}) Paper\n${SCISSORS_NUMBER}) Scissors\n${EXIT_NUMBER}) Quit`,
       ),
     );
 
     switch (c) {
-      case 1:
-      case 2:
-      case 3:
+      case ROCK_NUMBER:
+      case PAPER_NUMBER:
+      case SCISSORS_NUMBER:
+      case EXIT_NUMBER:
         return c;
       default:
         prefixTextPrompt = "Not a correct choice.";
@@ -35,4 +83,30 @@ function getHumanChoice() {
   }
 }
 
-function playRound(humanScore, computerScore) {}
+function roundAction(computerChoice, computerWinChoice, computerLoseChoice) {
+  if (computerChoice == computerWinChoice) {
+    return COMPUTER_WIN_NUMBER; // computer win
+  } else if (computerChoice == computerLoseChoice) {
+    return PLAYER_WIN_NUMBER; // player win
+  } else {
+    return TIE_NUMBER; // no one wins
+  }
+}
+
+function playRound(playerChoice, computerChoice) {
+  // based off of playerScore, extrapolate results from computerscore
+
+  switch (playerChoice) {
+    case ROCK_NUMBER: // rock
+      return roundAction(computerChoice, PAPER_NUMBER, SCISSORS_NUMBER);
+    case PAPER_NUMBER: // paper
+      return roundAction(computerChoice, SCISSORS_NUMBER, ROCK_NUMBER);
+    case SCISSORS_NUMBER: // scissors
+      return roundAction(computerChoice, ROCK_NUMBER, PAPER_NUMBER);
+    case EXIT_NUMBER:
+      return EXIT_NUMBER;
+    default:
+      console.error("Player choice is cooked");
+      return ERROR_NUMBER;
+  }
+}
